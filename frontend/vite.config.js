a36391e4be0dd49bd402b9b086ee93f7869464bd
Arbinniroul <arbinniroula21@gmail.com>
2025-04-03 22:11:06 +0545

    verceladd14

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['axios'] 
   },
+  build: {
+    rollupOptions: {
+      external: [], 
+      output: {
+        assetFileNames: 'assets/[name].[ext]' 
+      }
+    },
+    commonjsOptions: {
+      include: [/node_modules/] 
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src' 
+    }
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase'
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/variables.scss";`
+      }
+    }
+  }
 });
