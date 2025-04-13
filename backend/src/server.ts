2fe27f93bf4d6c8e8d96454ec553de918012ed9b
Arbinniroul <arbinniroula21@gmail.com>
2025-04-13 18:07:58 +0545

    ErrorFixed16

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 import visitorRoutes from './routes/visitorRoutes';
 import authRouter from './routes/authRouter';
 import connectDB from './connection/db';
-import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Middleware
+
 app.use(express.json());
 
 const allowedOrigins = [
-  'http://localhost:5173', 
+  'http://localhost:5173',
   'https://www.arbinniroula.com.np'
-  
 ];
+
 if (!allowedOrigins) {
-  console.error("CORS Error: 'process.env.URL' is not defined!");
+  console.error("CORS Error: Allowed origins are not defined!");
 }
 
 app.use(cors({
@@ -29,42 +29,33 @@ app.use(cors({
   credentials: true
 }));
 
-// Database connection
+
 connectDB().catch(err => {
   console.error('Database connection failed', err);
   process.exit(1);
 });
 
-
-app.use('/api/auth',authRouter); 
-app.use('/api', visitorRoutes);  
-
+// Routes
+app.use('/api/auth', authRouter);
+app.use('/api', visitorRoutes);
 
 app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../frontend/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
-  });
-}
 
 
 app.use((req, res) => {
-  res.status(404).json({ 
+  res.status(404).json({
     status: 'error',
-    message: 'Endpoint not found' 
+    message: 'Endpoint not found'
   });
 });
 
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 export default app;
